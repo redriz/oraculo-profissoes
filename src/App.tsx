3e66f23ce6813a3d63eeb13bfbcd4dcde89b7909
Adonai Rodriguez <rodrigz.dzn@gmail.com>
2026-04-14 15:27:00 +0100

    feat: enhance App component layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { InfoIcon } from "lucide-react"
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-2xl font-bold">Hello World</h1>
+      <div className="flex gap-1">
+        <InfoIcon />
+        <p>This is a React app with TypeScript.</p>
+      </div>
+      <Button>Its a button from Shadcn</Button>
     </>
   )
 }
