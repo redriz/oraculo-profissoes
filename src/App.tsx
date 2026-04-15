264fe53e476f53e70a8e428af0dadc79c8740af9
Adonai Rodriguez <rodrigz.dzn@gmail.com>
2026-04-15 10:39:51 +0100

    feat: add Inputs component and integrate it into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { InfoIcon } from "lucide-react"
 import { Button } from "./components/ui/button"
+import { Inputs } from "./components/inputs"
 
 function App() {
   return (
     <>
       <h1 className="text-2xl font-bold">Hello World</h1>
+      <Inputs />
       <div className="flex gap-1">
         <InfoIcon />
         <p>This is a React app with TypeScript.</p>
---
+++ src/components/inputs.tsx
@@ -0,0 +1,11 @@
+import { Input } from "./ui/input";
+
+function Inputs() {
+  return (
+    <div>
+        <Input placeholder="Type something..." />
+    </div>
+  );
+}
+
+export { Inputs }
