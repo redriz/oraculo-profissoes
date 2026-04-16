6bcf296edc89fca468796d76cb5e4ca957956aae
Adonai Rodriguez <rodrigz.dzn@gmail.com>
2026-04-16 15:02:27 +0100

    feat: implement Card component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
-import { InfoIcon } from "lucide-react"
-import { Button } from "./components/ui/button"
-import { Inputs } from "./components/inputs"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold">Hello World</h1>
-      <Inputs />
-      <div className="flex gap-1">
-        <InfoIcon />
-        <p>This is a React app with TypeScript.</p>
+      <div id="oraculo-app">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Oráculo das Profissões</CardTitle>
+            <CardDescription>
+              Descubra qual curso é o ideal para você
+            </CardDescription>
+            <CardAction>...</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="flex gap-2">
+              <Button className="flex-1">Sim</Button>
+              <Button variant="destructive" className="flex-1">
+                Não
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
       </div>
-      <Button>Its a button from Shadcn</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
